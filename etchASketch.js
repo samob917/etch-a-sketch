b58fdc45153444415a303bbab21db1b89c8dc55c
Sam Oberly <samob917@outlook.com>
2025-01-01 03:21:38 -0500

    Make 16 by 16 grid and add id to each element
    Added an id to each element and made the grid. The grid seems to
    display correctly, now moving to the next section.

--- etchASketch.js
+++ etchASketch.js
@@ -1,8 +1,9 @@
 let container = document.querySelector(".container");
 
-for (let i = 0; i < 64; i++) {
+for (let i = 0; i < 16*16; i++) {
     let newDiv = document.createElement("div");
     newDiv.classList.add("grid-element");
+    newDiv.setAttribute("id", `${i}`);
     newDiv.textContent = `${i}`;
     container.appendChild(newDiv);
 }
