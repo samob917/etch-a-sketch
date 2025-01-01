975de4f0f5ad7b7d5fb215de7a63b0702efc3cbb
Sam Oberly <samob917@outlook.com>
2025-01-01 04:22:18 -0500

    Make size of the board flexible
    Make the size of the board change from 16x16 to anythin under 100
    square units. This seems to work! I will now add a border around the
    board so its more obvious.

--- etchASketch.js
+++ etchASketch.js
@@ -1,12 +1,35 @@
 let container = document.querySelector(".container");
+let gridSize = 16;
 
-for (let i = 0; i < 16*16; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.classList.add("grid-element");
-    newDiv.setAttribute("id", `${i}`);
-    newDiv.textContent = `${i}`;
-    newDiv.addEventListener("mouseenter", function (e) {
-        e.target.style.background = "blue";
-    });
-    container.appendChild(newDiv);
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize*gridSize; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.classList.add("grid-element");
+        newDiv.setAttribute("id", `${i}`);
+        newDiv.addEventListener("mouseenter", function (e) {
+            e.target.style.background = "blue";
+        });
+        newDiv.style.width = `${95/gridSize}vw`;
+        newDiv.style.height = `${95/gridSize}vh`;
+        container.appendChild(newDiv);
+    }
 }
+
+
+createGrid(16);
+
+let button = document.querySelector("button");
+button.onclick = function () {
+    let requestedGridSize = Number(prompt("Enter number of squares per side, must be less than 100."));
+
+    if (!isNaN(requestedGridSize) && requestedGridSize < 100) {
+        gridSize = requestedGridSize;
+        console.log(`Changing grid size to ${gridSize} by ${requestedGridSize}.`);
+        container.replaceChildren();
+        createGrid(gridSize);
+    } else if (isNaN(requestedGridSize)) {
+        alert("Error: Must be a number.")
+    } else {
+        alert("Error: Must be less than 100.")
+    };
+};
