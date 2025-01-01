48cf0956c9fda4d8c3e04ea418b4b225fe158e9e
Sam Oberly <samob917@outlook.com>
2025-01-01 03:30:40 -0500

    Add mouseenter effect
    Added a mouseenter listener effect to change the background color
    of the divs as the mouse moves over them. This is working well!

--- etchASketch.js
+++ etchASketch.js
@@ -5,5 +5,8 @@ for (let i = 0; i < 16*16; i++) {
     newDiv.classList.add("grid-element");
     newDiv.setAttribute("id", `${i}`);
     newDiv.textContent = `${i}`;
+    newDiv.addEventListener("mouseenter", function (e) {
+        e.target.style.background = "blue";
+    });
     container.appendChild(newDiv);
 }
