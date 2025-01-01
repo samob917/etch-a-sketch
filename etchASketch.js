2d89b8c1de4a8b2bb7ee9000edc8a56e1eb25ce3
Sam Oberly <samob917@outlook.com>
2025-01-01 03:11:55 -0500

    Create 8x8 grid
    Created an 8x8 grid, realized that this is not ideal beacuase
    I wanted a 16 by 16 grid. Also, I want to generate the sized better.
    Going to try to add the css directly to each element.

--- etchASketch.js
+++ etchASketch.js
@@ -2,6 +2,7 @@ let container = document.querySelector(".container");
 
 for (let i = 0; i < 64; i++) {
     let newDiv = document.createElement("div");
+    newDiv.classList.add("grid-element");
     newDiv.textContent = `${i}`;
     container.appendChild(newDiv);
 }
